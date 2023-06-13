6e1f4239e979e80ffa29f529792255af1d9e89e9
SGDoubler <doublesn@mail.uc.edu>
2023-06-12 21:30:26 -0400

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner month = new Scanner(System.in);
+        System.out.println("What is your birth month 1-12?");
+        int birthMonth = month.nextInt();
+
+        if (birthMonth > 0 && birthMonth < 13)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+
+        }
+
     }
 }
